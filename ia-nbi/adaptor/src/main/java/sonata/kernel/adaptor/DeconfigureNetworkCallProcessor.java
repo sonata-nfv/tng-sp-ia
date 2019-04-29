8d8100a9f5e2de3f9685aa0f9646dec8be083a89
carlos-martins-marques <carlos-martins-marques@telecom.pt>
2019-04-29 12:04:57 +0100

    increase timeouts

--- ia-nbi/adaptor/src/main/java/sonata/kernel/adaptor/DeconfigureNetworkCallProcessor.java
+++ ia-nbi/adaptor/src/main/java/sonata/kernel/adaptor/DeconfigureNetworkCallProcessor.java
@@ -63,7 +63,7 @@ public class DeconfigureNetworkCallProcessor extends AbstractCallProcessor {
       resourceRepo.putResourcesForRequestId(message.getSid(),vendorSize);
     }
 
-    int wait = 15000;
+    int wait = 60000;
     try {
       Thread.sleep(wait);
     } catch (InterruptedException e) {
--- ia-nbi/adaptor/src/main/java/sonata/kernel/adaptor/ListComputeVimCallProcessor.java
+++ ia-nbi/adaptor/src/main/java/sonata/kernel/adaptor/ListComputeVimCallProcessor.java
@@ -60,7 +60,7 @@ public class ListComputeVimCallProcessor extends AbstractCallProcessor {
       resourceRepo.putResourcesForRequestId(message.getSid(),vendorSize);
     }
 
-    int wait = 15000;
+    int wait = 60000;
     try {
       Thread.sleep(wait);
     } catch (InterruptedException e) {
